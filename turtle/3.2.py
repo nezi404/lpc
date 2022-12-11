0187e12b870e3685db1628150c9ebc9044b3d967
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-11 13:41:20 -0300

    Create 3.2.py

---
+++ turtle/3.2.py
@@ -0,0 +1,44 @@
+# Importing turtle for the visual output
+from turtle import *
+
+speed("fastest")
+
+# Changing the direction of the turtle to go upwards
+rt(-90)
+
+# The angle between the main branch and the others
+angle = 30
+
+
+# Function to draw (plot) the branch/branches
+def branch_plotting(branch_size, branch_number):
+
+    # The variable branch_size are "sticks" of the tree
+    # The variable branch_number is how much
+    # "sticks" the tree has in each branch
+    if branch_number > 0:
+        # Color palette number range
+        colormode(255)
+        # Making a colour for the "sticks"
+        pencolor(0, 255 // branch_number, 0)
+        # Drawing the first stroke
+        fd(branch_size)
+        # Marking the next curvy
+        rt(angle)
+        # Making the next shorter and showing how many still lasts
+        branch_plotting(0.8 * branch_size, branch_number - 1)
+        pencolor(0, 255//branch_number, 0)
+        # Changing the angle of the turtle for a new branch
+        lt(2 * angle)
+        branch_plotting(0.8 * branch_size, branch_number - 1)
+        pencolor(0, 255//branch_number, 0)
+        rt(angle)
+        fd(-branch_size)
+
+
+# Asking the desired values to the user
+values = input("How much big and how many levels? ")
+branch_size, branch_number = values.split(" and ")
+branch_size = int(branch_size)
+branch_number = int(branch_number)
+branch_plotting(branch_size, branch_number)
