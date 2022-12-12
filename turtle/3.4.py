5b3bd90cf832126151b3a8aed10d89d9fb43f769
nezi404 <105327627+nezi404@users.noreply.github.com>
2022-12-12 03:02:53 -0300

    Update 3.4.py

--- turtle/3.4.py
+++ turtle/3.4.py
@@ -2,6 +2,7 @@
 import turtle
 import random
 
+
 """ Two players are able to play the game but,firstly, we make the 
 player one and after a clone for player two, giving it a different 
 colour and initial position.
@@ -15,7 +16,6 @@ player_two = player_one.clone()
 player_two.color("blue")
 player_two.penup()
 player_two.goto(-200, -100)
-
 # Making the finish line for each player
 # and taking them back to the start
 player_one.goto(300, 60)
@@ -31,30 +31,24 @@ player_two.circle(40)
 player_two.penup()
 player_two.goto(-200, -100)
 player_two.pendown()
-
 # Making a die with the usage of a list
 die = [1, 2, 3, 4, 5, 6]
 
-# Starting the process of the game, with a loop of 20, which would be
-# the minimum for any player to win a match
+# Starting the process of the game with a loop 
 for rounds in range(20):
     #  if any player hasn't reached any of the respective coordinates
     # the game will continue until it is reached by rolling the die
-
     if player_one.pos() >= (300, 100):
         print("Player One Wins!")
-
         break
 
     if player_two.pos() >= (300, -100):
         print("Player Two Wins!")
-
         break
 
     else:
-
         # Both player gets to roll the dice and make their
-        #  own turtle move to the coordinate given by the die
+        # own turtle move to the coordinate given by the die
         player_one_turn = input("Press 'Enter' to roll the die")
         die_roll_result = random.choice(die)
         print("The result of the die roll is: ")
